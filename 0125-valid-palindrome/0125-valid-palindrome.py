da5e9c37b0b19280025a31b878d8ae978664815a
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-02-21 19:02:52 -0500

    Time: 377 ms (6.51%), Space: 12.8 MB (82.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+
+        new = ''
+        for a in s:
+            if a.isalpha() or a.isdigit():
+                new += a.lower()
+        return (new == new[::-1])
