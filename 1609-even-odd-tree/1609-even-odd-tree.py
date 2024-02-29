b83c28d418d8ef5a5006fa0d332e2d8881dc39f4
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-02-28 19:39:15 -0500

    Time: 201 ms (95.88%), Space: 38 MB (56.17%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.py
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -1,15 +1,19 @@
 class Solution:
     def isEvenOddTree(self, root: TreeNode) -> bool:
-        even = 1 # even level 
+        even = 1
         queue = deque([root])
-        while queue: 
-            newq = []
+        while queue:
+            nodes = len(queue)
             prev = -inf if even else inf
-            for _ in range(len(queue)): 
+            for _ in range(nodes):
                 node = queue.popleft()
-                if even and (node.val&1 == 0 or prev >= node.val) or not even and (node.val&1 or prev <= node.val): return False 
+                if even and(node.val % 2 == 0 or prev >= node.val):
+                    return False
+                elif not even and(node.val % 2 == 1 or node.val >= prev):
+                    return False
                 prev = node.val 
                 if node.left: queue.append(node.left)
                 if node.right: queue.append(node.right)
             even ^= 1
         return True 
+                        
