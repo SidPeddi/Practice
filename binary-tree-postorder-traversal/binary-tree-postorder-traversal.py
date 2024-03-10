a6190944542f4e0c686e50436da4e1bc956745c2
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-03-09 23:15:02 -0500

    Time: 28 ms, Memory: 16.4 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -1,21 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def postorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        def dfs(node):
-            if not node: return
-            dfs(node.left)
-            dfs(node.right)
-            ans.append(node.val)
-            return   
-        ans = []
-        dfs(root)
-        return ans
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result=[]
+        stack=[]
+        while root or stack:
+            while root:
+                stack.append(root) # push nodes into the stack
+                root=root.left if root.left else root.right
+            root=stack.pop()
+            result.append(root.val) #Deal with the root node whenever it is popped from stack
+            if stack and stack[len(stack)-1].left==root: #check whether it has been traversed 
+                root=stack[len(stack)-1].right
+            else:
+                root=None #Force to quit the loop
+        return(result)
