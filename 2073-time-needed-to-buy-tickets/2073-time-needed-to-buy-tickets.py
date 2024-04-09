85beca60da34c619c846d22c105af9ef798ba075
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-04-09 12:41:34 -0400

    Time: 36 ms (90.62%), Space: 17 MB (5.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,16 +1,13 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         count = 0
-        i = 0
-        while tickets[k] > 0:
-            if i == len(tickets):
-                i = 0
-            if tickets[i] == 0:
-                i += 1
-                continue
-            count += 1
-            tickets[i] -= 1
-
-            i += 1
+        for i,v in enumerate(tickets):
+            print(count, i , v)
+            if v >= tickets[k]:
+                count += tickets[k]
+                if k < i:
+                    count -= 1
+            else:
+                count += v
         
         return count
