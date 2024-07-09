c2bac4028cee6e4a9c8a85fbc5e23e3807dff039
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-08 23:50:52 -0400

    Time: 39 ms (99.41%), Space: 13.3 MB (87.06%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -6,14 +6,17 @@ class Solution(object):
         :rtype: int
         """
         ans = 0
-        numset = set(nums)
-        counter = Counter(nums)
-        if k > 0:
-            for x in numset:
-                if (x+k) in numset:
+        myset = set(nums)
+
+        if k == 0:
+            maps = {}
+            for x in nums:
+                maps[x] = maps.get(x,0) + 1
+            for x in myset:
+                if maps[x] > 1:
                     ans += 1
         else:
-            for x in numset:
-                if counter[x] > 1:
+            for x in myset:
+                if (x-k) in myset:
                     ans += 1
         return ans
