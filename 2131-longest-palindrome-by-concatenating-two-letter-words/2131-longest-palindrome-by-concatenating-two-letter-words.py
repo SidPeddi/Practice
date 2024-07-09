d3a003584c0c1aea52d177c1173268612d76a892
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-08 22:46:10 -0400

    Time: 1265 ms (5.36%), Space: 35.9 MB (57.14%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -5,24 +5,22 @@ class Solution(object):
         :rtype: int
         """
         
-        
-        Hash = {}
-        doubleset = set()
-        ans = 0
+        maps = {}
+        count = 0
         for x in words:
-            if Hash.get(x[::-1]):
-                ans += 4
-                Hash[x[::-1]] = Hash.get(x[::-1],0) - 1
+            if x[::-1] in maps:
+                print(x)
+                count += 4
+                maps[x[::-1]] = maps.get(x[::-1],0) - 1
+                if maps[x[::-1]] == 0:
+                    del maps[x[::-1]]
             else:
-                Hash[x] = Hash.get(x,0) + 1
-            if x[::-1] == x:
-                if x in doubleset:
-                    doubleset.remove(x)
-                else:
-                    doubleset.add(x)
-        if doubleset:
-            ans += 2
-        return ans 
-                
-                
+                maps[x] = maps.get(x,0) + 1
+        for x in words:
+            if x in maps:
+                if x == x[::-1]:
+                    print(x)
+                    count += 2
+                    break
+        return count
         
