2360d9db20c9fcdeda9cc0ac59ebea9fa65e6722
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-11 00:13:44 -0400

    Time: 973 ms (5.79%), Space: 13.8 MB (25.62%) - LeetHub

---
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def largestPalindromic(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        count = Counter(num)
+        nums = list(count)
+        nums.sort(reverse = True)
+        ans = ''
+        mid = ''
+        for x in nums:
+            while count[x] >= 2:
+                ans += x
+                count[x] -= 2
+        for x in nums:
+            if count[x] >= 1:
+                mid = x
+                break
+        while ans:
+            if ans[0] == '0':
+                ans = ans[1:]
+            else:
+                break
+        if not ans and not mid:
+            return '0'
+        return ans + mid + ans[::-1]
+        
