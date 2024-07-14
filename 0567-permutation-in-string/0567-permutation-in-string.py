151bf26f0c4563b2ce5ee0193c33a95e93fd3ab9
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-13 22:13:55 -0400

    Time: 2010 ms (14.69%), Space: 16.6 MB (51.67%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = len(s1)
+        for x in range(len(s2)-r+1):
+            if len(s2[l+x:r+x]) == len(s1) and Counter(s1) == Counter(s2[l+x:r+x]):
+                return True
+        return False
+            
+
+                
+            
