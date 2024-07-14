8f05b18ac795eaf0158b432d052cf827d60d2e4a
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-13 20:10:04 -0400

    Time: 33 ms (72.40%), Space: 16.3 MB (94.12%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,17 +1,13 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         counts = Counter(s)
-        ans = []
+        ans = ''
         for c in order:
-            if c in counts:
-                while counts[c]:
-                    ans.append(c)
-                    counts[c] = counts.get(c,0) - 1
-                del counts[c]
-                s = s.replace(c, '')
-        temp = ''.join(ans)
-        temp += s
-        return temp
+            while counts[c]:
+                ans += c
+                counts[c] -= 1
+            s = s.replace(c, '')
+        return ans + s
                 
                 
                 
