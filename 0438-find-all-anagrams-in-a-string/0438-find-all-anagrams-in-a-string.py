e5efa1e111aa50f6d6e30ebe99e796fd64ecd1e8
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-26 11:17:40 -0400

    Time: 467 ms (16.93%), Space: 17.9 MB (22.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,10 +1,20 @@
-class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        r = len(p)
-        count = Counter(p)
-        ans = []
-        for l in range(len(s) - len(p) + 1):
-            if Counter(s[l:r+l]) == count:
-                ans.append(l)
-        return ans
+class Solution(object):
+    def findAnagrams(self, s, p):
+
+        n = len(s)
+        m = len(p)
         
+        p = Counter(p)                    # Convert list of anagram letters to a Counter (hashtable)
+        ans = []
+           
+        window = None
+        for i in range(0,n-m+1):
+            if i == 0: 
+                window = Counter(s[:m])   # Initialize window with beginning of s => length = length of anagram letters
+            else:    
+                window[s[i-1]] -= 1       # Move window to right: 1. remove character on the left
+                window[s[i+m-1]] += 1     #                       2. add character on the right
+            if len(window - p) == 0:      # Check if window is anagram (direct comparison of counters does not work with 0 entries)
+                ans.append(i)
+                
+        return ans
