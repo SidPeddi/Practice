be03b111baa92a6c2d394d5f22f8e72f63a7714a
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-07-27 11:30:52 -0400

    Time: 115 ms (90.41%), Space: 20.7 MB (44.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,20 +1,16 @@
 class Solution:
     
-    def do_overlap(self,interval_1, interval_2):
-        front = max(interval_1[0],interval_2[0])
-        back = min(interval_1[1], interval_2[1])      
-        return back - front >= 0
-    
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()   
         result = [intervals[0]]
+        
         for interval in intervals[1:]:
             interval_2 = result[-1]
             
-            if self.do_overlap(interval, interval_2):
-                merged_front = min(interval[0], interval_2[0])
+            if interval_2[1] >= interval[0]:
                 merged_back = max(interval[1], interval_2[1])
-                result[-1] = [merged_front, merged_back]
+                result[-1][1] = merged_back
             else:
                 result.append(interval)               
+        
         return result
