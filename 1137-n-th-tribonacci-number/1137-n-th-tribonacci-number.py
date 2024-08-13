056dad1fa1f0c57fef61a824547e70813a1cbcf7
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-08-12 20:34:33 -0400

    Time: 30 ms (83.68%), Space: 16.5 MB (57.52%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,15 +1,9 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = {}
-        memo[0] = 0
-        memo[1] = 1
-        memo[2] = 1
-        
-        def climb(n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n] = climb(n-1) + climb(n-2) + climb(n-3)
-                return memo[n]
-        return climb(n)
+        @cache
+        def t(n):
+            if n==0: return 0
+            if n<=2: return 1
+            return t(n-1)+t(n-2)+t(n-3)
+        return t(n)
         
