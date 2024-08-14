1c0875697ead35ec2d27b13182f9abd96aea5eb6
SidPeddi <160797685+SidPeddi@users.noreply.github.com>
2024-08-13 20:27:52 -0400

    Time: 30 ms (88.07%), Space: 17.2 MB (9.00%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,9 @@
 class Solution:
+    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def steps(m,n):
-            if m == 0 or n == 0:
-                return 0
-            if m == 1 and n == 1:
-                return 1
-            return steps(m-1,n) + steps(m,n-1)
-        return steps(m,n)
+        if m == 0 or n == 0:
+            return 0
+        if m == 1 and n == 1:
+            return 1
+        return self.uniquePaths(m-1,n) + self.uniquePaths(m,n-1)
         
